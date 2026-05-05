cbd61fd521f2fb53fa9eadf64429aa804dfe19bd
rabelasjohnlhoyd-del <rabelasjohnlhoyd@gmail.com>
2026-05-05 17:01:56 +0800

    feat: add no-cache headers to responses for improved security and caching control

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ app = reg_app()
 def Home():
     return redirect(url_for('auth.login'))
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
